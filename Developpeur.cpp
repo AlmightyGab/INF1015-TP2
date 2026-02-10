5cb1101519cf33fcf68ac7e6ce27bf63287853a5
Gabriel Tremblay <gtremblay@MacBookAir.home>
2026-02-10 13:19:11 -0500

    Developpeur class implemented

---
+++ Developpeur.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include "include/gsl/span"
+#include "Developpeur.hpp"
+#include "ListeJeux.hpp"
+
+using namespace std;
+
+
+
+Developpeur::Developpeur(const string& nom, const ListeJeux& listeJeux)
+    : nom_(nom), jeux_(listeJeux)
+{
+    // Paire nom-jeux
+    paireNomJeux_ = make_pair(nom, jeux_);
+}
+
+Developpeur::~Developpeur()
+{
+    delete [] jeux_.elements;
+}
+
+unsigned Developpeur::obtenirNombreJeux(const ListeJeux& listeJeux) const
+{
+    gsl::span<Jeu*> spanJeux(listeJeux.elements, listeJeux.nElements);
+    unsigned count = 0;
+    for (auto jeu : spanJeux) {
+        if (jeu->developpeur == nom_) 
+            ++count;
+    }
+    return count;
+}
+
+void Developpeur::miseAJourJeux(const ListeJeux& listeJeux)
+{
+    jeux_.nElements = 0;
+    jeux_.capacite = obtenirNombreJeux(listeJeux);
+
+    Jeu** jeuxTemp = new Jeu*[jeux_.capacite];
+    gsl::span<Jeu*> spanJeux(listeJeux.elements, listeJeux.nElements);
+
+    for (auto jeu : spanJeux) {
+        if (jeu->developpeur == nom_)
+            jeuxTemp[jeux_.nElements++] = jeu;
+    }
+
+    jeux_.elements = jeuxTemp;
+    delete [] jeuxTemp;
+}
+
+void Developpeur::afficherJeux() const
+{
+    const string separateur = "\n\033[36m────────────────────────────────────────\033[0m\n";
+
+    cout << separateur;
+    cout << "Voici les jeux de " << nom_ << " :\n";
+    for (unsigned i = 0; i < jeux_.nElements; ++i) 
+        cout << " -> " << jeux_.elements[i]->titre << "\n";
+    cout << separateur;
+}
+
+int main() {return 0;}
--- Developpeur.hpp
+++ Developpeur.hpp
@@ -3,12 +3,37 @@
 #include <string>
 #include <utility>
 #include "ListeJeux.hpp"
+#include "Jeu.hpp"
 
 class Developpeur
 {
 	//TODO: En faire une classe qui suit les principes OO.
 	//TODO: La destruction d'un Developpeur doit s'assurer que la désallocation de ListeJeux est faite.
 	//TODO: Les méthodes à faire...
+public:
+	// Conctructeur
+	Developpeur(const std::string& nom, const ListeJeux& listeJeux);
+	// Destructeur
+	virtual ~Developpeur();
 
+	// Methodes publiques
+	inline std::string obtenirNom() const {return nom_;};
+
+	inline unsigned obtenirNombreJeux() const {return jeux_.nElements;}
+	unsigned obtenirNombreJeux(const ListeJeux& listeJeux) const;
+
+	void miseAJourJeux(const ListeJeux& listeJeux);
+	
+	void afficherJeux() const;
+
+
+private:
+	// Attributs privés
 	std::pair<std::string, ListeJeux> paireNomJeux_;
+
+	/* Je ne suis pas sur de comprendre l'utilite de cette paire etant donne qu'on traite toujours les 
+    attributs du nom et de la liste de jeux du dev separement. J'ai donc choisi d'avoir ces attributs 
+    independants puisque c'est une bien meilleur pratique (universellement reconnue) en OOP. */
+	std::string nom_;
+	ListeJeux jeux_;
 };
